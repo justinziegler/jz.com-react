62dfbaa29ddccdc480fa22a21f58cb2106a3d22b
justinziegler <justin.ziegler@gmail.com>
2024-10-15 16:22:02 -0700

    Made Simple edits

--- src/pages/06-mattress/content/madeSimple.js
+++ src/pages/06-mattress/content/madeSimple.js
@@ -1,4 +1,6 @@
 import React, { useState, useContext, useEffect } from 'react';
+import { Swiper, SwiperSlide }                    from 'swiper/react';
+import { Pagination }                             from 'swiper';
 import Accordion                                  from 'react-bootstrap/Accordion';
 import AccordionContext                           from 'react-bootstrap/AccordionContext';
 import { useAccordionButton }                     from 'react-bootstrap/AccordionButton';
@@ -51,6 +53,8 @@ function MadeSimple(props) {
   ]
 
   const [activeIndex, setActiveIndex] = useState(0)
+  const mobile = window.innerWidth < 768
+
 
   return (
     <section className='made-simple'>
@@ -60,40 +64,69 @@ function MadeSimple(props) {
             <h2>Mattress Buying<br /> Made Simple</h2>
             <div className='frame' data-current={ activeIndex }>
               <div className='stage'>
-                <Accordion defaultActiveKey={ 0 } flush  key={ `accordion`}>
-                  { items.map((item, index) =>
-                    <React.Fragment key={ `accordion${ index }`}>
-                      <Card>
-                        <Card.Header>
-                          <ContextAwareToggle 
-                            eventKey={ index }
-                            setActiveIndex={ setActiveIndex }
-                          >
-                            { item.title }
-                          </ContextAwareToggle>
-                        </Card.Header>
-                        <Accordion.Collapse eventKey={ index }>
-                          <Card.Body>
-                            <div className='content'>
-                              <p>{ item.details }</p>
-                            </div>
-                          </Card.Body>
-                        </Accordion.Collapse>
-                      </Card>
-                    </React.Fragment>
-                  )}
-                </Accordion>
-              </div>
-              <div className='dots'>
-                { items.map((item, index) =>
-                  <button data-slide={ index } 
-                    data-active={ index === activeIndex } 
-                    tabIndex='0'
-                    key={ `dot${ index }`}
+                { mobile ?
+                  <>
+                    <Swiper 
+                      modules={[Pagination]} 
+                      pagination={{
+                        el: '.simple.swiper-pagination',
+                        clickable: true,
+                      }}
+                      loop={ true }
+                      grabCursor={ true }
+                      centeredSlides={ true }
                     >
-                  </button>
-                )}
+                      { items.map((item, index) =>
+                        <SwiperSlide key={ `slideshow-${ index }` }>
+                          <div className='content'>
+                            <h4>{ item.title }</h4>
+                            <p>{ item.details }</p>
+                          </div>
+                        </SwiperSlide>
+                      )}
+                    </Swiper>
+                    
+                  </>
+                :
+                  <Accordion defaultActiveKey={ 0 } flush  key={ `accordion`}>
+                    { items.map((item, index) =>
+                      <React.Fragment key={ `accordion${ index }`}>
+                        <Card>
+                          <Card.Header>
+                            <ContextAwareToggle 
+                              eventKey={ index }
+                              setActiveIndex={ setActiveIndex }
+                            >
+                              { item.title }
+                            </ContextAwareToggle>
+                          </Card.Header>
+                          <Accordion.Collapse eventKey={ index }>
+                            <Card.Body>
+                              <div className='content'>
+                                <p>{ item.details }</p>
+                              </div>
+                            </Card.Body>
+                          </Accordion.Collapse>
+                        </Card>
+                      </React.Fragment>
+                    )}
+                  </Accordion>
+                }
               </div>
+              { mobile ?
+                <div className='simple swiper-pagination'></div>
+              :
+                <div className='dots'>
+                  { items.map((item, index) =>
+                    <button data-slide={ index } 
+                      data-active={ index === activeIndex } 
+                      tabIndex='0'
+                      key={ `dot${ index }`}
+                      >
+                    </button>
+                  )}
+                </div>
+              }
             </div>
           </div>
         </div>
